4f8edcb4e2a84d37e87731601a1fe593e9df6916
PurpleRevenue77 <25853967-PurpleRevenue77@users.noreply.replit.com>
2023-09-21 15:37:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,249 @@
+import turtle
+turtle.tracer(0, 0)
+turtle.up()
+turtle.goto(-300, -200)
+turtle.down()
+
+turtle.left(35)
+turtle.forward(20)
+turtle.left(60)
+turtle.forward(25)
+turtle.right(95)
+turtle.forward(40)
+
+turtle.up()
+turtle.goto(-245, -175)
+turtle.down()
+turtle.right(90)
+turtle.circle(45, -180)
+turtle.color()
+turtle.up()
+turtle.goto(-155, -165)
+turtle.down()
+turtle.right(90)
+turtle.forward(47)
+turtle.right(50)
+turtle.circle(60, -60)
+turtle.left(110)
+turtle.forward(150)
+turtle.right(240)
+turtle.circle(60, -60)
+turtle.right(61)
+turtle.forward(84)
+turtle.up()
+turtle.goto(115, -175)
+turtle.down()
+turtle.right(83)
+turtle.circle(45, -190)
+turtle.up()
+turtle.goto(205, -167)
+turtle.down()
+turtle.right(85)
+turtle.forward(60)
+turtle.right(35)
+turtle.forward(15)
+turtle.right(55)
+turtle.forward(15)
+turtle.right(90)
+turtle.forward(40)
+turtle.up()
+turtle.goto(207, -180)
+turtle.down()
+turtle.left(90)
+turtle.up()
+turtle.goto(278, -190)
+turtle.down()
+turtle.right(45)
+turtle.forward(25)
+turtle.up()
+turtle.goto(278, -190)
+turtle.down()
+turtle.right(135)
+turtle.forward(117)
+
+turtle.circle(20, 60)
+turtle.left(30)
+turtle.forward(380)
+turtle.right(135)
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(150)
+turtle.right(45)
+turtle.forward(70)
+turtle.circle(-50, 30)
+turtle.right(105.5)
+turtle.forward(290)
+turtle.left(90)
+turtle.forward(46)
+turtle.left(90)
+turtle.forward(400)
+
+
+def rectangle():
+  'Draw a rectangle'
+  for i in range(2):
+    turtle.left(90)
+    turtle.forward(45)
+    turtle.left(90)
+    turtle.forward(250)
+rectangle()
+turtle.up()
+turtle.goto(-110,-61)
+turtle.down()
+turtle.right(180)
+turtle.forward(5)
+for i in range(10):
+  turtle.left(1)
+  turtle.forward(15)
+for p in  range(11):
+  turtle.left(9)
+  turtle.forward(5)
+turtle.forward(20)
+turtle.right(110)
+for m in  range(11):
+  turtle.left(1)
+  turtle.forward(5)
+turtle.left(90)
+turtle.forward(20)
+turtle.left(80)
+turtle.forward(60)
+turtle.up()
+turtle.goto(-276,-139)
+turtle.down()  
+turtle.right(10)
+turtle.forward(45)
+turtle.up()
+turtle.goto(-117,-60)
+turtle.down() 
+turtle.left(58)
+turtle.forward(102)
+turtle.right(48)
+turtle.forward(163)
+turtle.right(48)
+turtle.forward(102)
+turtle.up()
+turtle.goto(-120,-60)
+turtle.down()
+for m in  range(2):
+  turtle.right(3)
+  turtle.forward(-1)
+turtle.right(120)
+turtle.up()
+turtle.goto(-120,-110)
+turtle.down()
+turtle.right(6)
+turtle.forward(168)
+turtle.up()
+turtle.goto(-120,-60)
+turtle.down()
+for t in  range(3):
+  turtle.right(9)
+  turtle.forward(7)
+turtle.right(100)
+for q in  range(3):
+  turtle.right(5)
+  turtle.forward(40)
+turtle.right(37)
+turtle.forward(150)
+for a in  range(40):
+  turtle.right(1.25)
+  turtle.forward(0.75)
+turtle.forward(90)
+turtle.left(48)
+turtle.forward(35)
+turtle.left(30)
+turtle.forward(35)
+turtle.right(35)
+turtle.forward(32)
+turtle.right(95)
+turtle.forward(22)
+turtle.right(90)
+turtle.forward(22)
+turtle.right(180)
+turtle.forward(2)
+turtle.right(2)
+for f in  range(3):
+  turtle.right(5)
+  turtle.forward(8)
+turtle.right(55)
+turtle.up()
+turtle.goto(276,-105)
+turtle.down()
+turtle.right(95)
+turtle.forward(15)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(15)
+turtle.up()
+turtle.goto(35,-165)
+turtle.down()
+turtle.left(178)
+turtle.forward(150)
+turtle.up()
+turtle.goto(22,-61)
+turtle.down()
+turtle.right(100)
+turtle.forward(80)
+turtle.up()
+turtle.goto(26,-61)
+turtle.down()
+turtle.forward(70)
+turtle.right(80)
+turtle.forward(60)
+turtle.right(45)
+turtle.forward(60)
+turtle.up()
+turtle.goto(145,0)
+turtle.down()
+turtle.right(230)
+turtle.forward(200)
+for x in range (12):
+  turtle.left(90)
+  turtle.forward(50)
+  turtle.left(80)
+  turtle.forward(10*(i+2))
+turtle.up()
+turtle.goto(-67,-63)
+turtle.down()
+turtle.right(220)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(30)
+
+
+
+
+
+
+turtle.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Project Proposal
+
+1. Roughly, what will your drawing look like
+i am going to create an outline of a BMW e30m3
+
+2. What are two variables you could use so that when you change each variable, the drawing changes?
+
+"def" and" for i in range (to repeat something)
+
+
+
+
+
+
+
+
+![BMW_E30_M3_Zeichung_schatten](BMW_E30_M3_Zeichung_schatten.png)
+
+3. Where is there repetition in your drawing?
+yes, on the tires and on the general frame of the car. 
+
+
+4. What are the main parts of your drawing? Are there clear sections or objects?
+
+tire frame, windows and the general frame of the car which is the rest of it. 
+
+
+5. How could you use a loop variable of a for loop? Is there somewhere in your drawing where something is repeated, but it's slightly different each time? Maybe the same object is repeated in different places, or maybe a similar object is repeated but with different sizes?
+
+a place I could use a loop varible is the tires. they have the exact same dimensions. besides that, everything is slightly different than everything else.
+
+
+
+
+
+
